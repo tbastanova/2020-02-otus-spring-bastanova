366c95a400949456a975c5118f968b562059dd03
Bastanova Tatiana <tbastanova@gmail.com>
2020-03-18 23:08:12 +0300

    Домашнее задание к занятию №1

---
+++ homework-01/src/test/java/ru/otus/spring/domain/StudentTest.java
@@ -0,0 +1,27 @@
+package ru.otus.spring.domain;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Класс Student")
+class StudentTest {
+    private static final String FIRST_NAME="Иван";
+    private static final String LAST_NAME="Иванов";
+
+    @DisplayName("Имя задано корректно")
+    @Test
+    void getFirstName() {
+        Student student = new Student();
+        student.setFirstName(FIRST_NAME);
+        assertEquals(FIRST_NAME,student.getFirstName());
+    }
+
+    @DisplayName("Фамилия задана корректно")
+    @Test
+    void getLastName() {
+        Student student = new Student();
+        student.setLastName(LAST_NAME);
+        assertEquals(LAST_NAME,student.getLastName());
+    }
+}
