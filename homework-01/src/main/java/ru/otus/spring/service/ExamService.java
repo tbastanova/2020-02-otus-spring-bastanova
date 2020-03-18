270c9421d339c48990cf0b57a99f510291616bef
Bastanova Tatiana <tbastanova@gmail.com>
2020-03-19 00:25:56 +0300

    -

--- homework-01/src/main/java/ru/otus/spring/service/ExamService.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.service;
-
-import java.io.IOException;
-
-public interface ExamService {
-    void startExam() throws IOException;
-}
--- homework-01/src/main/java/ru/otus/spring/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.service;
-
-import ru.otus.spring.domain.Student;
-
-import java.io.IOException;
-
-public interface StudentService {
-    Student register() throws IOException;
-}
