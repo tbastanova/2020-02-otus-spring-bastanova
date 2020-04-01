6c822bae001850dc2a6c062e737d298ce2398310
Bastanova Tatiana <tbastanova@gmail.com>
2020-04-01 21:16:00 +0300

    Исправила замечания. Получение application.properties перенесла в LocalProps В конфиге оставила бин ввода/вывода, на случай если что-то отличное от консоли.

--- homework-02/src/main/java/ru/otus/spring/config/ServiceConfig.java
+++ homework-02/src/main/java/ru/otus/spring/config/ServiceConfig.java
@@ -2,14 +2,11 @@ package ru.otus.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import ru.otus.spring.service.IOContext;
 import ru.otus.spring.service.IOService;
 import ru.otus.spring.service.impl.ConsoleContextImpl;
 import ru.otus.spring.service.impl.IOServiceImpl;
 
-
-@PropertySource("classpath:application.properties")
 @Configuration
 public class ServiceConfig {
 
--- homework-02/src/main/java/ru/otus/spring/service/impl/StudentServiceImpl.java
+++ homework-02/src/main/java/ru/otus/spring/service/impl/StudentServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.spring.service.impl;
 
 import org.springframework.stereotype.Service;
 import ru.otus.spring.domain.Student;
+import ru.otus.spring.service.IOService;
 import ru.otus.spring.service.LocalizationService;
 import ru.otus.spring.service.StudentService;
 
@@ -10,7 +11,7 @@ public class StudentServiceImpl implements StudentService {
     private ru.otus.spring.service.IOService ioService;
     private final LocalizationService localizationService;
 
-    public StudentServiceImpl(ru.otus.spring.service.IOService ioService, LocalizationService localizationService) {
+    public StudentServiceImpl(IOService ioService, LocalizationService localizationService) {
         this.ioService = ioService;
         this.localizationService = localizationService;
     }
